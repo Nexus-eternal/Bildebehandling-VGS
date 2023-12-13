9031a41802c706247c11096ba6fcfcfcd36078b2
Nexus-eternal <133900069+Nexus-eternal@users.noreply.github.com>
2023-12-13 11:08:23 +0100

    added picture finding; added test picture

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from PIL import Image
 import pilgram
+import os
 
+def mainloop():
+    run = True
+    files = []
+    print("Wellcome to ACFE!")
+    print("Choose the picture you want to edit:")
+    counter = 1
+    for file in os.scandir('.'):
+        if file.is_file() and file.path.split(".")[-1].lower() in ['png','jpg','jpeg']:
+            files.append(file)
+            print(f'{counter}. {file.name}')
+
+
+    while run:
+        pass
+
+mainloop()
