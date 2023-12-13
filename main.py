dd967e3c79985a6c615d4afc61b2e702087e4b48
Nexus-eternal <133900069+Nexus-eternal@users.noreply.github.com>
2023-12-13 14:21:44 +0100

    restuctured code; added class

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from PIL import Image
 import pilgram
 import os
 
-def mainloop():
-    run = True
-    files = []
-    print("Wellcome to ACFE!")
-    print("Choose the picture you want to edit:")
-    counter = 1
-    for file in os.scandir('.'):
-        if file.is_file() and file.path.split(".")[-1].lower() in ['png','jpg','jpeg']:
-            files.append(file)
-            print(f'{counter}. {file.name}')
+class Program():
+
+    def __init__(self) -> None:
+        files = []
+        print("Wellcome to ACFE!")
+        print("Choose the picture you want to edit:" + "\n")
+        counter = 1
+        for file in os.scandir('.'):
+            if file.is_file() and file.path.split(".")[-1].lower() in ['png','jpg','jpeg']:
+                files.append(file)
+                print(f'{counter}. {file.name}')
+        print("\n")
+        file = input("File you will work with: ")
+        file = int(file)
+        file = files[file-1]
+
+    def mainloop(self):
+        run = True
+        print("ABOBA")
 
 
-    while run:
-        pass
 
-mainloop()
+pr = Program()
+pr.mainloop()
