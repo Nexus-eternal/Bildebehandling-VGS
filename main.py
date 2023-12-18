5bb6e8f8e940c4de9cde1d08691c27f4cb119152
Nexus-eternal <133900069+Nexus-eternal@users.noreply.github.com>
2023-12-18 07:35:10 +0100

    added code for mainbody;

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 class Program():
 
     def __init__(self) -> None:
+        commands = []
+        self.commands = commands
         files = []
         print("Wellcome to ACFE!")
         print("Choose the picture you want to edit:" + "\n")
@@ -13,14 +15,23 @@ class Program():
             if file.is_file() and file.path.split(".")[-1].lower() in ['png','jpg','jpeg']:
                 files.append(file)
                 print(f'{counter}. {file.name}')
+                counter += 1
         print("\n")
         file = input("File you will work with: ")
         file = int(file)
         file = files[file-1]
+        self.file = file
 
     def mainloop(self):
         run = True
-        print("ABOBA")
+        print("What do you want to do with picture?")
+        counter = 1
+        for command in self.commands:
+            print(f"{counter}. {command}")
+        im = Image.open(self.file.name)
+        pilgram.moon(im).save(f'{self.file.name}-moon.jpg')
+
+
 
 
 
